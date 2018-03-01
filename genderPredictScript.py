ac8eb257daf637ef5b9253a6cfff5116ad3e6084
Ganesh Chaitanya Kale <ganesh2583@gmail.com>
2018-03-01 21:59:29 +0530

    Added 2 more clasifiers and refined the test data set used.

--- genderPredictScript.py
+++ genderPredictScript.py
@@ -1,21 +1,57 @@
 from sklearn import tree
+from sklearn import neighbors
+from sklearn import gaussian_process
 
 #[height, weight, shoe size]
-X = [[181,80,44],[565,57,57],[131,57,75],[32,35,576],[75,57,57],[57,34,57],[34,34,34],[87,97,55],[45,67,54],[23,80,64],[77,57,23]]
+X = [[181,80,10],[161,70,6],[171,66,7],[176,88,7],[189,100,8],[141,80,5],[156,78,6],[161,50,6],[171,60,7],[151,78,7],[171,40,7]]
 #Gender
-Y = ['female','female','male','female','male','male','male','male','female','female','female']
+Y = ['male','male','male','male','male','female','female','female','female','female','female']
 
-#Define Classifier From the imported Tree
-classifier = tree.DecisionTreeClassifier()
+#Define 'DecisionTreeClassifier' Classifier From the imported Tree
+decisionTreeclassifier = tree.DecisionTreeClassifier()
 
 #Fit the data into the Classifier
-classifier = classifier.fit(X,Y)
+decisionTreeclassifier = decisionTreeclassifier.fit(X,Y)
 
 #Perform Prediction
-prediction = classifier.predict([[10,67,90]])
+decisionTreeclassifierPrediction = decisionTreeclassifier.predict([[161,60,9]])
 
 #Print the Classifier
-print(classifier)
+print(decisionTreeclassifier)
 
 #Print the Prediction
-print(prediction)
+print(decisionTreeclassifierPrediction)
+
+
+
+#Define 'KNeighborsClassifier' Classifier From the imported Tree
+kNeighborsClassifier = neighbors.KNeighborsClassifier()
+
+#Fit the data into the Classifier
+kNeighborsClassifier = kNeighborsClassifier.fit(X,Y)
+
+#Perform Prediction
+kNeighborsClassifierPrediction = kNeighborsClassifier.predict([[161,60,9]])
+
+#Print the Classifier
+print(kNeighborsClassifier)
+
+#Print the Prediction
+print(kNeighborsClassifierPrediction)
+
+
+
+#Define 'GaussianProcessClassifier' Classifier From the imported Tree
+gaussianProcessClassifier = gaussian_process.GaussianProcessClassifier()
+
+#Fit the data into the Classifier
+gaussianProcessClassifier = gaussianProcessClassifier.fit(X,Y)
+
+#Perform Prediction
+gaussianProcessClassifierPrediction = gaussianProcessClassifier.predict([[161,60,9]])
+
+#Print the Classifier
+print(gaussianProcessClassifier)
+
+#Print the Prediction
+print(gaussianProcessClassifierPrediction)
